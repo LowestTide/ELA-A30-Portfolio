5f01123496a0c4ab87c8d80df6a6ef47371d5f6f
Pranav Dhokia <pranavdhokia24@gmail.com>
2026-06-15 13:24:36 -0600

    Add files via upload
    Added a variety of CSS upgrades with some minor JavaScript tweaks. Next commit will likely be the final one of this project

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+const facetCards = document.querySelectorAll('.facet-card');
+const facetCardClasses = ["facet-card-content-upper-right", "facet-card-content-upper-left"];
+const facetCardClassesRight = ["facet-card-content-right-left", "facet-card-content-right-right"];
+
+facetCards.forEach(card => {
+    
+    if(card.id == "oppression" || card.id == "autonomy"){
+        
+        for(let i = 0; i < card.children.length; i++){
+            console.log(card.children[i]);
+            card.children[i].classList.remove(facetCardClasses[0], facetCardClasses[1]);
+            card.children[i].classList.add(facetCardClassesRight[i]);
+            console.log(card.children[i]);
+        }
+        console.log(card.children);
+    }
+    card.addEventListener("mouseover", event => {
+        
+        
+    })
+})
